8d7c0ffa0dafae58b37a0e85ca49178ece89a995
Samuel Ytterbrink <neppord@gmail.com>
2018-09-09 16:57:34 +0200

    First impl of first assignment, needs to read file.

---
+++ 1.hs
@@ -0,0 +1,15 @@
+#! /usr/bin/env stack
+-- stack --resolver lts-12.9 script
+import Graphics.Gloss
+
+windowWidth = 640
+windowHeight = 480
+windowDimensions = (windowWidth, windowHeight)
+window = InWindow "Hello World" windowDimensions (10, 10)
+background = white
+picture 
+  = Translate (-170) (-20)
+  $ Scale 0.5 0.5
+  $ Text "Hello World"
+main = display window background picture
+
