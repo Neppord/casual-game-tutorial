9e32810c7ef12c736b0df7b9c287c37d369be2c2
Samuel Ytterbrink <neppord@gmail.com>
2018-10-13 14:58:08 +0200

    Derivie show and eq for data types.

--- hs/Four/Data.hs
+++ hs/Four/Data.hs
@@ -5,10 +5,10 @@ import Graphics.Gloss
 data Drop = Drop
   { pos :: Point
   , mass :: Float
-  }
+  } deriving (Show, Eq)
 
 data State = State
   { bg :: Picture
   , rainDrop :: Drop
-  }
+  } deriving (Show, Eq)
 
