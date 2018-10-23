3f5ab304901eae5c7067f581221e56471b23f700
Samuel Ytterbrink <neppord@gmail.com>
2018-10-23 10:41:39 +0200

    Try some TDD on the rain physics.

--- hs/Four/PhysicsSpec.hs
+++ hs/Four/PhysicsSpec.hs
@@ -1,16 +1,25 @@
 module Four.PhysicsSpec (spec) where
 
+import Prelude hiding ((-))
+
+import Graphics.Gloss.Data.Point.Arithmetic ((-))
 import Test.Hspec
 
 import Four.Data
 
 fall :: Float -> Drop -> Drop
-fall height drop = drop {pos=(-3, 0)}
+fall height drop = drop {pos=newPos}
+  where
+    newPos = pos drop - (height, 0)
 
 spec = 
   describe "fall" $ do
+    let zerroDrop = Drop {pos=(0,0), mass=0}
+    let oldDrop = Drop {pos=(-4,0), mass=0}
     it "decrement the height of a drop by a fixed amount" $ do
-      let drop = Drop {pos=(0,0), mass=0}
-      let result = fall 3 drop
-      let expected = Drop {pos=(-3, 0), mass=0}
-      result `shouldBe` expected
+      fall 3 zerroDrop `shouldBe`
+        Drop {pos=(-3, 0), mass=0}
+      fall 2 zerroDrop `shouldBe`
+        Drop {pos=(-2, 0), mass=0}
+      fall 2 oldDrop `shouldBe`
+        Drop {pos=(-6, 0), mass=0}
