c24ac4c0b6e6560d626ab4a9efd0002a87dbb813
Samuel Ytterbrink <neppord@gmail.com>
2019-08-08 19:21:54 +0200

    Copy challange 5 to 6 and use a rotated image.

---
+++ elm/src/a6.elm
@@ -0,0 +1,209 @@
+module Main exposing (main)
+
+import Browser
+import Browser.Events exposing (onAnimationFrameDelta, onClick)
+import Html exposing (div, text)
+import Html.Attributes
+import Json.Decode as Decode
+import Random
+import Svg exposing (g, image, svg)
+import Svg.Attributes
+
+
+canvasWidth =
+    640
+
+
+canvasHeight =
+    480
+
+
+speed =
+    10.0
+
+
+main =
+    Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+subscriptions _ =
+    Sub.batch
+        [ onAnimationFrameDelta Tick
+        , onClick (Decode.map2 Shot (Decode.field "x" Decode.float) (Decode.field "y" Decode.float))
+        ]
+
+
+type alias Milliseconds =
+    Float
+
+
+type Msg
+    = Tick Milliseconds
+    | Shot Float Float
+    | NewBall Ball
+
+
+type alias Ball =
+    { x : Float, y : Float, r : Float, dx : Float, dy : Float }
+
+
+generateBall : Float -> Random.Generator Ball
+generateBall r =
+    let
+        genX =
+            Random.float r (canvasWidth - r)
+
+        genY =
+            Random.float r (canvasHeight - r)
+
+        genD =
+            Random.float (negate speed) speed
+
+        ball x y dx dy =
+            { x = x, y = y, r = r, dx = dx, dy = dy }
+    in
+    Random.map4 ball genX genY genD genD
+
+
+init () =
+    ( ( 0
+      , { x = 200.0
+        , y = 200.0
+        , r = 200.0
+        , dx = 1.0
+        , dy = 1.0
+        }
+      )
+    , Cmd.none
+    )
+
+
+view ( score, { x, y, r } ) =
+    let
+        raddie =
+            String.fromFloat r
+    in
+    div
+        [ Html.Attributes.style "user-select" "none"
+        , Html.Attributes.style "color" "darkgreen"
+        , Html.Attributes.style "font-size" "21px"
+        , Html.Attributes.style "font-family" "Arial"
+        ]
+        [ text ("Score: " ++ String.fromInt score)
+        , svg
+            [ Svg.Attributes.width <| String.fromInt canvasWidth
+            , Svg.Attributes.height <| String.fromInt canvasHeight
+            , Svg.Attributes.style "position: absolute; top: 0; left: 0; background-color: lightblue; z-index: -1"
+            ]
+            [ g
+                [ Svg.Attributes.transform
+                    ("translate("
+                        ++ String.fromFloat (x - r)
+                        ++ ","
+                        ++ String.fromFloat (y - r)
+                        ++ ")"
+                    )
+                ]
+                [ image
+                    [ Svg.Attributes.xlinkHref "ball.png"
+                    , Svg.Attributes.width <| String.fromFloat (r * 2)
+                    , Svg.Attributes.height <| String.fromFloat (r * 2)
+                    , Svg.Attributes.transform ("rotate(180, " ++ raddie ++ ", " ++ raddie ++ ")")
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+type alias Collision =
+    ( Order, Order )
+
+
+rangeEqual { start, stop } value =
+    case ( compare value start, compare value stop ) of
+        ( LT, _ ) ->
+            LT
+
+        ( _, GT ) ->
+            GT
+
+        ( _, _ ) ->
+            EQ
+
+
+hCollision { x, r } =
+    rangeEqual { start = r, stop = canvasWidth - r } x
+
+
+vCollision { y, r } =
+    rangeEqual { start = r, stop = canvasHeight - r } y
+
+
+projectModel delta ({ x, dx, y, dy } as model) =
+    { model | x = x + delta * 0.1 * dx, y = y + delta * 0.1 * dy }
+
+
+nextDelta c d =
+    case c of
+        LT ->
+            abs d
+
+        GT ->
+            negate <| abs d
+
+        EQ ->
+            d
+
+
+withInBall ball x y =
+    let
+        a =
+            abs (x - ball.x)
+
+        b =
+            abs (y - ball.y)
+
+        c =
+            sqrt ((a ^ 2) + (b ^ 2))
+    in
+    c < ball.r
+
+
+update msg ( score, ball ) =
+    case msg of
+        Tick delta ->
+            ( ( score, updateBall delta ball ), Cmd.none )
+
+        Shot x y ->
+            ( ( score, ball )
+            , if withInBall ball x y then
+                Random.generate NewBall <| generateBall (ball.r * 0.95)
+
+              else
+                Cmd.none
+            )
+
+        NewBall newBall ->
+            ( ( score + 1, newBall ), Cmd.none )
+
+
+updateBall delta model =
+    let
+        project =
+            projectModel delta
+
+        projectedModel =
+            project model
+
+        hc =
+            hCollision projectedModel
+
+        vc =
+            vCollision projectedModel
+    in
+    project
+        { model
+            | dx = nextDelta hc model.dx
+            , dy = nextDelta vc model.dy
+        }
